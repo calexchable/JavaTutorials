576b446ac125b0e1e0e08031063819cb754e6c0b
Christopher Alex Chable <calexchable@gmail.com>
2020-01-09 08:23:48 -0600

    Started App for my Java Tutorials

---
+++ src/main/java/com/xochable/JavaTutorials/JavaTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.xochable.JavaTutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xochable/JavaTutorials/JavaTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xochable.JavaTutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
