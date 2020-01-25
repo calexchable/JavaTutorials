380fe5dca34a49aaae1230f001d8747d08868574
Christopher Alex Chable <calexchable@gmail.com>
2020-01-25 10:00:26 -0600

    Finished Where To examples with external js files, calex.

--- src/main/resources/static/myScript.js
+++ src/main/resources/static/myScript.js
@@ -1,3 +1,7 @@
 function myFunctionEx() {
 document.getElementById("demoJSdoc").innerHTML = "Paragraph changed from external doc.";
 }
+
+function myFunctionExTwo() {
+document.getElementById("demoJSdocTwo").innerHTML = "Paragraph2 changed2 from2 external2 doc2.";
+}
