4c4746c463f203eea428c7b55267ab5ab028fffd
Christopher Alex Chable <calexchable@gmail.com>
2020-01-31 19:21:20 -0600

    Refactor: created JSTutorial to hold all JSTutorial exercises, calex.

---
+++ src/main/java/com/xochable/JavaTutorials/controllers/JSTutorialController.java
@@ -0,0 +1,50 @@
+package com.xochable.JavaTutorials.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class JSTutorialController {
+
+    @RequestMapping(value = "intro")
+    public String intro(Model model) {
+
+        model.addAttribute("title", "Introduction");
+
+        return "intro";
+    }
+
+    @RequestMapping(value = "where-to")
+    public String whereTo(Model model) {
+
+        model.addAttribute("title", "Where To");
+
+        return "whereTo";
+    }
+
+    @RequestMapping(value = "output")
+    public String output(Model model) {
+
+        model.addAttribute("title", "Output");
+
+        return "output";
+    }
+
+    @RequestMapping(value = "statement")
+    public String statement(Model model) {
+
+        model.addAttribute("title", "Statement");
+
+        return "statements";
+    }
+
+    @RequestMapping(value = "syntax")
+    public String syntax(Model model) {
+
+        model.addAttribute("title", "Syntax");
+
+        return "syntax";
+    }
+}
